8787cdc768e1bf4b2d1a865d5107ece1ca4102f3
AdvithDesu <75376191+AdvithDesu@users.noreply.github.com>
2021-10-22 11:42:45 +0530

    Add files via upload

---
+++ Lightkurve_ExoplanetProject.py
@@ -0,0 +1,66 @@
+import pandas as pd
+import lightkurve as lk
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Enter the start file number and end file number in the input screen
+# Example if you want to analyse from file 3 to file 10 you have to enter "3 10" (without the quotes ofc)
+csv_number = list(map(int, input("Enter the range of files you wish to analyse: ").split(" ")))
+# If you want to start analysis from somewhere in the middle
+mid_break = int(input("Enter the index from which u wish to analyse: "))
+
+for file_no in range(csv_number[0], csv_number[1] + 1):
+
+    # Change the download_dir to the directory where you saved your excel sheet in
+    # download_dir = "{folder directory in your pc}" + "\\StarData_" + str(file_no) + ".csv"
+    # Note that you have to only change the text in the {} brackets, below is an example of my download_dir
+    download_dir = "C:\\Users\\advi\\Downloads" + "\\StarData_" + str(file_no) + ".csv"
+    print(f"Analysing File-{file_no}")
+
+    # Change the output_dir to the directory where you want the figures to be saved
+    # Below is an example of my output_dir:
+    output_dir = "D:\\test export"
+
+    # Note that we skip the first 4 rows from any of the excel sheets as they are not important
+    df = pd.read_csv(download_dir, skiprows=4)
+
+    for i in range(mid_break, df.shape[0] if file_no == csv_number[0] else df.shape[0]):
+
+        starstr = "TIC " + str(df.iloc[i][0])
+        print(f"Analysing index-{i}")
+
+        try:
+            initial = lk.search_lightcurve(starstr)  # Searching the data for the a given "starstr"
+            rows = len(initial)
+        except:
+            print(f"{starstr}[all] skipped")
+            continue
+
+        # This is the program to analyse each subset data a particular "starstr"
+        for row in range(rows):
+
+            try:
+                file1 = initial[row].download(quality_bitmask="default").remove_nans()
+                temporary_file = file1.remove_outliers(sigma=6).flatten()
+            except:
+                print(f"{starstr}[{rows}] skipped")
+                continue
+
+            # We use two seperate files to be analysed, as a result we get 2 figures for every subset
+            # This is because the "to_periodogram" function does not do a good job in finding the accurate time period
+            # The method we use does not guarentee a 100% accuracy either
+            periodogram_file_1 = temporary_file.to_periodogram(method="bls")
+            periodogram_file_2 = temporary_file.to_periodogram(method="bls", period=np.arange(1, 16, 0.01))
+
+            time_period_file1 = periodogram_file_1.period_at_max_power
+            time_period_file2 = periodogram_file_2.period_at_max_power
+
+            final_file_1 = temporary_file.fold(period=time_period_file1)
+            final_file_2 = temporary_file.fold(period=time_period_file2)
+
+            final_file_1.scatter()
+            plt.savefig(output_dir + "\\" + starstr + f"[{row}][normal].png")  # Plot 1
+
+            final_file_2.scatter()
+            plt.savefig(output_dir + "\\" + starstr + f"[{row}][arange].png")  # Plot 2
+            plt.close('all')  # This is important to include otherwise the system might crash
---
+++ WebScrapping.py
@@ -0,0 +1,92 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as ec
+from selenium.webdriver.common.keys import Keys
+import time
+
+
+# This path might be different for you. Make sure you set the right path for the driver.
+PATH = "C:\\Program Files (x86)\\chromedriver.exe"
+driver = webdriver.Chrome(PATH)
+
+
+def Get_from_RA(times):
+
+    # variables:
+    start = times / 2
+    end = start + 0.5
+    index = times + 1
+
+    try:
+        WebDriverWait(driver, 15).until(
+            ec.element_to_be_clickable((By.ID, "textfield-1208-inputEl")))
+    except:
+        driver.quit()
+
+    # Selecting the first input box to enter the interval
+    input_start = driver.find_element_by_id("textfield-1208-inputEl")
+    input_start.clear()
+    input_start.send_keys(str(start))  # Starting interval
+    input_start.send_keys(Keys.RETURN)
+
+    # Selecting the second input box to enter the interval
+    input_end = driver.find_element_by_id("textfield-1209-inputEl")
+    input_end.clear()
+
+    # There seems to a bug where input_start gets copied to input_end
+    for x in range(0 if start == 0 else len(str(start))):
+        input_end.send_keys(Keys.BACKSPACE)
+    input_end.send_keys(str(end))  # Ending interval
+    input_end.send_keys(Keys.RETURN)
+
+    # Selecting the first export button
+    export_button = WebDriverWait(driver, 10).until(
+        ec.presence_of_element_located((
+            By.XPATH, "/html/body/div[10]/div[3]/div/div[1]/div/div/div/a[2]/span/span/span[2]"
+        ))
+    )
+    export_button.click()
+
+    for j in range(15):
+        try:
+            file_name = driver.find_element_by_xpath("/html/body/div[" + str(
+                j) + "]/div[3]/div/div/div/div/fieldset/div/div/div/div/div/div/div/div[1]/\
+                div[1]/div[1]/div/input")
+            file_name.clear()
+            file_name.send_keys("StarData_" + str(index) + ".csv" + Keys.RETURN)
+            driver.find_element_by_xpath("/html/body/div[" + str(j) + "]/div[3]/div/div/div/\
+                div/fieldset/div/div/div/div/div/div/div/div[6]/div/div/a[1]/span/span/span[2]").click()
+            break
+
+        except:
+            continue
+
+# Opening the MAST website
+driver.get("https://mast.stsci.edu/portal/Mashup/Clients/Mast/Portal.html")
+
+# clicking the first drop-down arrow
+drag_button1 = driver.find_element_by_id("combo-1111-trigger-picker")
+drag_button1.click()
+
+# selecting 'MAST Catalog' option in the first drop-down
+drag_button1_option = driver.find_element_by_id("liActionChooserTopBar_MASTCATALOG")
+drag_button1_option.click()
+
+# clicking the second drop-down arrow
+drag_button2 = WebDriverWait(driver, 10).until(
+    ec.presence_of_element_located((By.ID, "combo-1114-trigger-picker"))
+)
+
+drag_button2.click()
+
+# selecting 'TESS CTL v8.01' option in the second drop-down
+drag_button2_option = driver.find_element_by_id("liMastCatalogChooserTopBar_Ctl")
+drag_button2_option.click()
+time.sleep(1)
+
+# Clicking on the "Advanced Search" option
+driver.find_element_by_link_text("Advanced Search").click()
+
+for i in range(5):
+    Get_from_RA(i)
