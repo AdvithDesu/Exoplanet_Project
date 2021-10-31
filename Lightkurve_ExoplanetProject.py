76dcdff44b9b69b96ca87a51eb539d88d4834ebe
AdvithDesu <75376191+AdvithDesu@users.noreply.github.com>
2021-10-31 19:33:04 +0530

    Update Lightkurve_ExoplanetProject.py

--- Lightkurve_ExoplanetProject.py
+++ Lightkurve_ExoplanetProject.py
@@ -2,6 +2,7 @@ import pandas as pd
 import lightkurve as lk
 import numpy as np
 import matplotlib.pyplot as plt
+import sys
 
 # Enter the start file number and end file number in the input screen
 # Example if you want to analyse from file 3 to file 10 you have to enter "3 10" (without the quotes ofc)
@@ -12,9 +13,9 @@ mid_break = int(input("Enter the index from which u wish to analyse: "))
 for file_no in range(csv_number[0], csv_number[1] + 1):
 
     # Change the download_dir to the directory where you saved your excel sheet in
-    # download_dir = "{folder directory in your pc}" + "\\StarData_" + str(file_no) + ".csv"
-    # Note that you have to only change the text in the {} brackets, below is an example of my download_dir
-    download_dir = "C:\\Users\\advi\\Downloads" + "\\StarData_" + str(file_no) + ".csv"
+    # input_dir = "{folder directory in your pc}" + "\\StarData_" + str(file_no) + ".csv"
+    # Note that you have to only change the text in the { } brackets, below is an example of my input_dir
+    input_dir = "C:\\Users\\advi\\Downloads" + "\\StarData_" + str(file_no) + ".csv"
     print(f"Analysing File-{file_no}")
 
     # Change the output_dir to the directory where you want the figures to be saved
@@ -22,26 +23,30 @@ for file_no in range(csv_number[0], csv_number[1] + 1):
     output_dir = "D:\\test export"
 
     # Note that we skip the first 4 rows from any of the excel sheets as they are not important
-    df = pd.read_csv(download_dir, skiprows=4)
+    df = pd.read_csv(input_dir, skiprows=4)
 
     for i in range(mid_break, df.shape[0] if file_no == csv_number[0] else df.shape[0]):
 
         starstr = "TIC " + str(df.iloc[i][0])
-        print(f"Analysing index-{i}")
+        print(f"Analysing index - {i}")
 
         try:
             initial = lk.search_lightcurve(starstr)  # Searching the data for the a given "starstr"
             rows = len(initial)
+        except KeyboardInterrupt:
+            sys.exit(print(f"Last file analysed is - {file_no}, on index {i}"))
         except:
             print(f"{starstr}[all] skipped")
             continue
 
-        # This is the program to analyse each subset data a particular "starstr"
+        # This is the program to analyse each subset data of a particular "starstr"
         for row in range(rows):
 
             try:
                 file1 = initial[row].download(quality_bitmask="default").remove_nans()
-                temporary_file = file1.remove_outliers(sigma=6).flatten()
+                temporary_file = file1.remove_outliers(sigma=4).flatten()
+            except KeyboardInterrupt:
+                sys.exit(print(f"Last file analysed is - {file_no}, on index {i}"))
             except:
                 print(f"{starstr}[{rows}] skipped")
                 continue
